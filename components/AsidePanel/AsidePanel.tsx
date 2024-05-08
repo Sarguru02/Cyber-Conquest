2c04b306452bd8daec7321215404ec7c3ab9a75b
Naveen Kumar <mnkincit22@gmail.com>
2024-05-08 09:39:45 +0530

    Refactor of Participants to AsidePanel

--- components/AsidePanel/AsidePanel.tsx
+++ components/AsidePanel/AsidePanel.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Participants = () => {
+const AsidePanel = () => {
   return (
     <div className="h-full w-2/5 flex flex-col space-y-10 bg-indigo-900 rounded-xl"></div>
   );
 };
 
-export default Participants;
+export default AsidePanel;
--- components/Board/Board.tsx
+++ components/Board/Board.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Canvas from "../Canvas/testcanvas";
-import Participants from "../Participants/Participants";
+import AsidePanel from "../AsidePanel/AsidePanel";
 //import Canvas from '../Canvas/canvas'
 
 const Board = () => {
   return (
     <div className="w-screen h-screen flex justify-around align-center p-2">
       <Canvas />
-      <Participants />
+      <AsidePanel />
     </div>
   );
 };
